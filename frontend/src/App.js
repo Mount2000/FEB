4451b7df81d290168ea1e5ad775eb210313ea07b
HanKorea <nguyennamhoai01042002@gmail.com>
2024-07-16 12:07:06 +0700

    config layout

---
+++ frontend/src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import NotFoundPage from "./pages/404";
+import AdminPage from "./pages/admin";
+import HomePage from "./pages/home";
+import Node from "./pages/node";
+import DefaultLayout from "./layouts";
+const App = () => {
+  return (
+    <BrowserRouter>
+      <DefaultLayout>
+        <Routes>
+          <Route path="*" element={<NotFoundPage />} />
+          <Route path="/" element={<HomePage />} />
+          <Route path="/node" element={<Node />} />
+          <Route path="/admin" element={<AdminPage />} />
+        </Routes>
+      </DefaultLayout>
+    </BrowserRouter>
+  );
+};
+
+export default App;
---
+++ frontend/src/components/Footer/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const BachiSwapFooter = () => {
+  return <div>Footer</div>;
+};
+
+export default BachiSwapFooter;

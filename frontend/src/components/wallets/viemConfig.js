eec9c9afbd284fec3499d6dcc3d0b1607901bae3
trung-dev <trungezeal115@gmail.com>
2024-08-17 20:53:53 +0700

    [fe] [be] update config

--- frontend/src/components/wallets/viemConfig.js
+++ frontend/src/components/wallets/viemConfig.js
@@ -1,6 +1,15 @@
 import { createPublicClient, http } from "viem";
-import { taikoHekla, holesky } from "viem/chains";
+import { taikoHekla, holesky, base } from "viem/chains";
+
+const chain = {
+  testnet: {
+    CHAIN: holesky,
+  },
+  mainnet: {
+    CHAIN: base,
+  },
+};
 export const taikoHeklaClient = createPublicClient({
-  chain: holesky,
+  chain: chain[process.env.REACT_APP_ENV || "testnet"].CHAIN,
   transport: http(),
 });
